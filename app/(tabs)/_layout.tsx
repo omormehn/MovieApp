7c2e05e435c23dd48dc5aa41a39e91dd8d18ad9f
omormehn <nathanuseni@gmail.com>
2025-03-13 11:50:39 +0100

    First commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,46 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Iconn from 'react-native-vector-icons/Entypo';
+const _Layout = () => {
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name='index'
+                options={{
+                    tabBarIcon: ({ size, color }) => {
+                        return <Icon name="home" size={size} color={color} />
+                    },
+                    headerShown: false
+                }} />
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+            <Tabs.Screen
+                name='games'
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="gamepad" size={size} color={color} />
+                    ),
+                    headerShown: false
+                }} />
+            <Tabs.Screen
+                name='news'
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Iconn name="news" size={size} color={color} />
+                    ),
+                    headerShown: false
+                }} />
+            <Tabs.Screen
+                name='profile'
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="user" size={size} color={color} />
+                    ),
+                    headerShown: false
+                }} />
+        </Tabs>
+    )
 }
+
+export default _Layout
---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const MovieDetail = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default MovieDetail;
